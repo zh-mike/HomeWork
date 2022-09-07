321e1fc70e33c5e640587da81a830966988a7ec1
zh-mike <zh-mike@mail.ru>
2022-09-07 17:08:18 +0500

    Первая есть

--- CSharpSeminar3/ex19/Program.cs
+++ CSharpSeminar3/ex19/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Палидром
+Console.Clear();
+Console.Write("Введите 5и значное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num /10000 == num %10 || (num / 1000) %10 == (num / 10) % 10) 
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+
