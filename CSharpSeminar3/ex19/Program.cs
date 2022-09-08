ec5fc2cf5635e37c151fa2b7580a97a558f510f2
zh-mike <zh-mike@mail.ru>
2022-09-09 01:49:27 +0500

    Исправил логически оператор

--- CSharpSeminar3/ex19/Program.cs
+++ CSharpSeminar3/ex19/Program.cs
@@ -3,7 +3,7 @@ Console.Clear();
 Console.Write("Введите 5и значное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-if (num /10000 == num %10 || (num / 1000) %10 == (num / 10) % 10) 
+if (num /10000 == num %10 && (num / 1000) %10 == (num / 10) % 10) 
 {
     Console.WriteLine("Yes");
 }
