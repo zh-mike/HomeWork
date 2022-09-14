f9d2aacfa6db5912471ee11f1c144492c3ad8e3c
zh-mike <zh-mike@mail.ru>
2022-09-15 00:57:43 +0500

    Сделал основные задания

---
+++ CSharpSeminar5/ex34/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+void RandomArrey(int[] arrey)
+{
+    for (int i = 0; i < arrey.Length; i++)
+    {
+        arrey[i] = new Random().Next(100,1000);
+    }
+}
+
+int Count(int[] arrey)
+{
+    int count = 0;
+    for (int i = 0; i < arrey.Length; i++)
+    {
+        if(arrey[i]%2 == 0) count++;
+    }
+    return count;
+}
+
+
+int[] myArrey = new int[5];
+RandomArrey(myArrey);
+Console.WriteLine("["+string.Join(", ", myArrey)+"]");
+Console.WriteLine("Количетсво четных чисел в массиве: "+ Count(myArrey));
---
+++ CSharpSeminar5/ex36/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+void RandomArrey(int[] arrey)
+{
+    for (int i = 0; i < arrey.Length; i++)
+    {
+        arrey[i] = new Random().Next(0, 16);
+    }
+}
+
+int Sum(int[] arrey)
+{
+    int sum = 0;
+    for (int i = 0; i < arrey.Length; i++)
+    {
+        if(i % 2 != 0) sum += arrey[i];
+    }
+    return sum;
+}
+
+int[] myArrey = new int[5];
+RandomArrey(myArrey);
+Console.WriteLine("["+string.Join(", ", myArrey)+"]");
+Console.WriteLine("Сумму элементов, стоящих на нечётных позициях: "+ Sum(myArrey));
