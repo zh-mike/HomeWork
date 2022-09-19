ef7e11a4200cb71476c9d1c33ee5bd66abca044e
zh-mike <zh-mike@mail.ru>
2022-09-19 23:56:53 +0500

    Решил две из трех

---
+++ CSharpSeminar6/ex41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Сколько чисел будите вводить: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] arrey = new int[m];
+int sum = 0;
+for (int i = 0; i < arrey.Length; i++)
+{
+    Console.Write("Введите число: ");
+    arrey[i] = Convert.ToInt32(Console.ReadLine());
+    if(arrey[i]>0) sum++;
+}
+Console.WriteLine("[" + string.Join (", ", arrey) + "]");
+Console.WriteLine(sum);
+
+
+
+
+
+
