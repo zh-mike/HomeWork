f084b38b8be2ca2b1ce24e402ad211a49dd42662
ZhMike <zh-mike@mail.ru>
2022-09-27 22:38:08 +0500

    Первый кривой вариант задачи 54

---
+++ CSharpSeminar8/ex54/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+// Меняем первую строку двумерного массива с последней
+Console.Clear();
+
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(0, 10);
+    }
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write(matrix[i, j] + " \t");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+void ReleaseMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            int k = j;
+            int max = matrix[i, j];
+            int maxK = j;
+            int x = 0;
+            while (k < matrix.GetLength(1))
+            {
+                if (max < matrix[i, k])
+                {
+                    max = matrix[i, k];
+                    maxK = k;
+                }
+                k++;
+            }
+            if (j!=maxK)
+            {
+                x = matrix[i, j];
+                matrix[i, j] = matrix[i, maxK];
+                matrix[i, maxK] = x;
+            }
+
+        }
+    }
+}
+
+
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+InputMatrix(matrix);
+Console.WriteLine("Исходный двумерный массив: ");
+PrintMatrix(matrix);
+ReleaseMatrix(matrix);
+Console.WriteLine("Конечный двумерный массив: ");
+PrintMatrix(matrix);
+
+
